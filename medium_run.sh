b699e4c42b970aefb1b25a83e708a956da571892
Chris Battarbee <chrisbattarbee3@gmail.com>
2020-05-28 19:40:19 +0100

    Add convenience scripts

---
+++ medium_run.sh
@@ -0,0 +1,1 @@
+bin/parsecmgmt -a run -p blackscholes canneal ferret fluidanimate freqmine streamcluster swaptions vips x264 -i simmedium
--- pgo_instrument_build.sh
+++ pgo_instrument_build.sh
@@ -1,4 +1,4 @@
 CXX_FLAGS="-O2 -fprofile-instr-generate -Xclang -fprofile-instrument=llvm"
 C_FLAGS="-O2 -fprofile-instr-generate -Xclang -fprofile-instrument=llvm"
 
-bin/parsecmgmt -a run -p blackscholes canneal ferret fluidanimate freqmine streamcluster swaptions vips x264 -i simmedium
+bin/parsecmgmt -a build -c gcc -p blackscholes canneal ferret fluidanimate freqmine streamcluster swaptions vips x264
