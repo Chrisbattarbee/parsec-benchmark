7bc9344cbe0c2131ff1293edd3a9b0008c29ae85
Chris Battarbee <chrisbattarbee3@gmail.com>
2020-05-28 21:11:17 +0100

    Update convenience scripts

--- pgo_instrument_build.sh
+++ pgo_instrument_build.sh
@@ -1,4 +1,10 @@
-CXX_FLAGS="-O2 -fprofile-instr-generate -Xclang -fprofile-instrument=llvm"
-C_FLAGS="-O2 -fprofile-instr-generate -Xclang -fprofile-instrument=llvm"
+export CXXFLAGS="-O2 -fprofile-instr-generate -Xclang -fprofile-instrument=llvm"
+export CXXFLAGS="-O2 -fprofile-instr-generate -Xclang -fprofile-instrument=llvm"
+export CFLAGS="-O2 -fprofile-instr-generate -Xclang -fprofile-instrument=llvm"
+export CFLAGS="-O2 -fprofile-instr-generate -Xclang -fprofile-instrument=llvm"
+export CC="/data/llvm-project/llvm/build/bin/clang"
+export LDFLAGS='-fprofile-instr-generate -Xclang -fprofile-instrument=llvm'
 
+bin/parsecmgmt -a clean
+bin/parsecmgmt -a uninstall 
 bin/parsecmgmt -a build -c gcc -p blackscholes canneal ferret fluidanimate freqmine streamcluster swaptions vips x264
